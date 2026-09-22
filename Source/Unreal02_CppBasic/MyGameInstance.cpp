8dd11619f8f4c109c686faf28e622e5b41e9647c
koo1805 <koo1805@naver.com>
2026-09-22 15:08:37 +0900

    FString / FName

--- Source/Unreal02_CppBasic/MyGameInstance.cpp
+++ Source/Unreal02_CppBasic/MyGameInstance.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyGameInstance.h"
@@ -9,5 +9,48 @@ void UMyGameInstance::Init()
 
 	// Output Log
 	// Unreal use UTF16 -> WCHAR
-	UE_LOG(LogTemp, Log, TEXT("Hello Unreal"));
+	// 각종 타입으로 변경할 수 있는 헬퍼 함수 제공
+	//UE_LOG(LogTemp, Log, TEXT("Hello Unreal"));
+
+	// TCHAR | FString
+	TCHAR LogCharArray[] = TEXT("Hello Unreal");
+	UE_LOG(LogTemp, Log, TEXT("%s"), LogCharArray);
+
+	FString LogCharString = LogCharArray;
+	UE_LOG(LogTemp, Log, TEXT("%s"), *LogCharString);
+
+	// 문자열 복사
+	TCHAR LogCharArrayWithSize[100] = {};
+	FCString::Strncpy(LogCharArrayWithSize, *LogCharString, LogCharString.Len());
+
+	UE_LOG(LogTemp, Log, TEXT("%s"), LogCharArrayWithSize);
+
+	// 문자열 자르기
+	if (LogCharString.Contains(TEXT("unreal"), ESearchCase::IgnoreCase))
+	{
+		// 문자열을 검색해 시작 인덱스 얻기
+		int32 Index = LogCharString.Find(TEXT("unreal"), ESearchCase::IgnoreCase);
+		FString EndString = LogCharString.Mid(Index);
+		UE_LOG(LogTemp, Log, TEXT("%s"), *EndString);
+	}
+
+	// 문자열 나누기
+	FString Left, Right;
+	if (LogCharString.Split(TEXT(" "), &Left, &Right))
+	{
+		UE_LOG(LogTemp, Log, TEXT("Split Test: %s / %s"), *Left, *Right);
+	}
+
+	// 문자열 조합
+	int32 IntValue = 40;
+	float FloatValue = 3.141592f;
+
+	FString FloatIntString = FString::Printf(TEXT("Int: %d, Float: %f"), IntValue, FloatValue);
+	UE_LOG(LogTemp, Log, TEXT("%s"), *FloatIntString);
+
+	// FName 특성 - 대소문자 구별하지 않음
+	FName Key01(TEXT("PELVIS"));
+	FName Key02(TEXT("pelvis"));
+
+	UE_LOG(LogTemp, Log, TEXT("FName 비교 결과: %s"), (Key01 == Key02 ? TEXT("같음") : TEXT("다름")));
 }
